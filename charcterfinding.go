c966832667cb880ccddf6f394cc35df169963436
sreelekshmi <mtmsreelekshmimg@gmail.com>
2021-07-02 17:33:24 +0530

    implement data type finder

--- charcterfinding.go
+++ charcterfinding.go
@@ -1,24 +1,41 @@
 package main
- import "fmt"
- 
-func find(ch interface{}){
-	switch ch.(type) {
-	case int:
-		fmt.Println("Type:int,vaue:",ch.(int))
-	case string:
-		fmt.Println("Type:string,value:",ch.(string))
-	case float64:
-		fmt.Println("Type:float64,value:",ch.(float64))
-	default:
-		fmt.Println("Type not found")
-		
-	}
-} 
- func main(){
-	var ch interface{}
-    fmt.Println("enter your charcater")
-	fmt.Scanln(&ch)
-	
 
- }
- 
+import (
+	"fmt"
+	"reflect"
+	"strings"
+)
+func main() {
+	var arr []interface{}
+	var data string
+	fmt.Println("enter your charcater")
+	fmt.Scanln(&data)
+	if strings.Contains(data, ".") {
+		arr = append(arr, 0.0)
+	}
+	if strings.Contains(data, "true") || strings.Contains(data, "false") {
+		arr = append(arr, true)
+	}
+	arr = append(arr, 0, "")
+	var value interface{}
+	var err error
+	for _, v := range arr {
+		value, err = convertType(data, v)
+		if err == nil {
+			break
+		}
+	}
+	fmt.Printf("Data Type of %s is :%T\n", data, value)
+
+}
+
+func convertType(data string, v interface{}) (interface{}, error) {
+	a := reflect.TypeOf(v)
+	value := reflect.New(a)
+
+	_, err := fmt.Sscan(data, value.Interface())
+	if err != nil {
+		fmt.Print("error", err)
+	}
+	return value.Elem().Interface(), err
+}
